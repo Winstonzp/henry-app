a1a01d7950520935b1c909dfdcecb1487a7e91c8
Winstonzp <zhangzhengping.zp@gmail.com>
2019-05-24 15:39:10 +0630

    final update layout and edit livegame page for menu tab

--- src/components/LiveGames.vue
+++ src/components/LiveGames.vue
@@ -7,24 +7,54 @@
         </v-card>
       </v-flex>
     </v-layout>
-    <v-layout style="background-color:#916001" row>
+    <v-layout class="#916001" style="height:100vh" row>
       <v-flex class="yellow darken-2" xs3>
         <div
           v-for="item in items"
           :key="item.title"
-          :class="{brown: current==item.title}"
+          :class="{ myRedbrown: current==item.title}"
           class="myMargin"
         >
           <div class="leftPadding py-3" @click="current = item.title">{{item.title}}</div>
         </div>
       </v-flex>
-      <v-flex xs9>
-        <v-layout v-for="s in sss" :key="s.title" row>
+      <v-flex xs9 v-if="current=='电子'">
+        <v-layout v-for="tabOne in tabOnes" :key="tabOne.title" row>
           <v-flex class="pl-3" xs6>
-            <v-img width="115px" height="85px" contain :src="s.pic"></v-img>
+            <v-img width="115px" height="85px" contain :src="tabOne.pic"></v-img>
           </v-flex>
           <v-flex width="115px" height="85px" class="pt-2" xs6>
-            <span style="color:orange">{{s.title}}</span>
+            <span style="color:orange">{{tabOne.title}}</span>
+            <v-flex class="pt-1">
+              <v-btn round small color="error">
+                <span style="color:white">开始游戏</span>
+              </v-btn>
+            </v-flex>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs9 v-if="current=='棋牌'">
+        <v-layout v-for="tabTwo in tabTwos" :key="tabTwo.title" row>
+          <v-flex class="pl-3" xs6>
+            <v-img width="115px" height="85px" contain :src="tabTwo.pic"></v-img>
+          </v-flex>
+          <v-flex width="115px" height="85px" class="pt-2" xs6>
+            <span style="color:orange">{{tabTwo.title}}</span>
+            <v-flex class="pt-1">
+              <v-btn round small color="error">
+                <span style="color:white">开始游戏</span>
+              </v-btn>
+            </v-flex>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs9 v-if="current=='真人'">
+        <v-layout v-for="tabThree in tabThrees" :key="tabThree.title" row>
+          <v-flex class="pl-3" xs6>
+            <v-img width="115px" height="85px" contain :src="tabThree.pic"></v-img>
+          </v-flex>
+          <v-flex width="115px" height="85px" class="pt-2" xs6>
+            <span style="color:orange">{{tabThree.title}}</span>
             <v-flex class="pt-1">
               <v-btn round small color="error">
                 <span style="color:white">开始游戏</span>
@@ -53,7 +83,7 @@ export default {
         { title: "关于" }
       ],
       right: null,
-      sss: [
+      tabOnes: [
         { pic: require("@/assets/casino.jpg"), title: "SW电子" },
         { pic: require("@/assets/casino.jpg"), title: "MG电子" },
         { pic: require("@/assets/casino.jpg"), title: "XJJ电子" },
@@ -64,6 +94,23 @@ export default {
         { pic: require("@/assets/casino.jpg"), title: "LGD电子" },
         { pic: require("@/assets/casino.jpg"), title: "PSG电子" }
       ],
+      tabTwos: [
+        { pic: require("@/assets/casino.jpg"), title: "ABC电子" },
+        { pic: require("@/assets/casino.jpg"), title: "DEF电子" },
+        { pic: require("@/assets/casino.jpg"), title: "HIJ电子" },
+        { pic: require("@/assets/casino.jpg"), title: "KLM电子" },
+        { pic: require("@/assets/casino.jpg"), title: "OPQ电子" }
+      ],
+      tabThrees: [
+        { pic: require("@/assets/casino.jpg"), title: "RST电子" },
+        { pic: require("@/assets/casino.jpg"), title: "UV电子" },
+        { pic: require("@/assets/casino.jpg"), title: "XR电子" },
+        { pic: require("@/assets/casino.jpg"), title: "XS电子" },
+        { pic: require("@/assets/casino.jpg"), title: "S5电子" },
+        { pic: require("@/assets/casino.jpg"), title: "RS电子" },
+        { pic: require("@/assets/casino.jpg"), title: "QOP电子" }
+      ],
+
       current: "电子"
     };
   }
@@ -82,5 +129,8 @@ export default {
 .myMargin {
   margin-top: 1px;
   margin-bottom: 1px;
+}
+.myRedbrown {
+  background-color: #916001;
 }
 </style>
